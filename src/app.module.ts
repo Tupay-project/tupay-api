1b7cb6a9dca679a7061b971d0db2cca693fd3ceb
flowercordoba <flowercordoba7@gmail.com>
2024-10-07 07:39:06 -0500

    addned new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 
-
+import {  PgModule } from './shared/modules';
+import { RolesService } from './features/role/role.service';
+import { ProviderModule } from './features/provider/provider.module';
+import { CustomerModule } from './features/customer/customer.module';
+import { InvoicesModule } from './features/invoices/invoices.module';
+import { InterbankModule } from './features/integrations/interbank/interbank.module';
+import { TokuModule } from './features/integrations/toku/toku.module';
+import { AuthModule } from './features/auth/auth.module';
+import { EmailModule } from './shared/modules/email/email.module';
 
 
 @Module({
   imports: [
+    //  config
+    PgModule,
+    ProviderModule,
+    CustomerModule,
+    InvoicesModule,
+    InterbankModule,
+    TokuModule,
+    AuthModule,
+    EmailModule
+    // features
+
+
   ],
   controllers: [],
-  providers: [AppService],
+  providers: [AppService,RolesService],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,34 +1,19 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
+import { RolesService } from './features/role/role.service';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly rolesService: any) {}
+    constructor(private readonly rolesService: RolesService) {}
 
-  async onModuleInit() {
-    const roles = await this.rolesService.listAllRoles();
-
-    // Si no existen roles, inicialízalos
-    if (roles.length === 0) {
-      await this.rolesService.initializeRolesAndPermissions();
-      console.log('Roles and Permissions initialized');
-    } else {
-      console.log('Roles and Permissions already initialized');
-    }
-
-    // Crear rol provider manualmente si no está creado
-    await this.rolesService.createRole('provider', [
-      'view_provider_transactions',
-      'confirm_payment',
-      'view_provider_balance',
-      'manage_provider_payments',
-      'view_provider_invoices',
-      'generate_invoice',
-      'pay_invoice',
-      'configure_webhook',
-      'view_webhook_logs',
-      'manage_provider_integrations',
-      'view_provider_audit_logs',
-    ]);
-    console.log('Provider role initialized');
-  }
+    async onModuleInit() {
+        const roles = await this.rolesService.listAllRoles();
+  
+        // validacion si no existe los roles y permisos en la db, los crea y si no pues no para que?
+        if (roles.length === 0) {
+          await this.rolesService.initializeRolesAndPermissions();
+          console.log('Roles and Permissions initialized');
+        } else {
+          console.log('Roles and Permissions already initialized');
+        }
+      }
 }
